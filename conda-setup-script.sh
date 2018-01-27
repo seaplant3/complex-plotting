b745ae5d0a23576f3546a4b8c523b31bd3f4bfe6
seaplant3 <35381909+seaplant3@users.noreply.github.com>
2018-01-26 22:52:32 -0800

    Update conda-setup-script.sh

--- conda-setup-script.sh
+++ conda-setup-script.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
-export PATH="$HOME/miniconda/bin:$PATH"
-conda create -n complexPlottingEnv python=2.7
-conda install -n complexPlottingEnv -y mayavi jupyter 
+export PATH="$HOME/miniconda2/bin:$HOME/miniconda/bin:$PATH"
+conda create -y -n complexPlottingEnv python=2.7
+conda install -y -n complexPlottingEnv mayavi jupyter 
 export PATH="$(conda info --root)/envs/complexPlottingEnv/bin:$PATH"
 cd "$(dirname "$(find $HOME -type f -name complex-plotting-jupyter-notebook.ipynb | head -1)")"
 jupyter notebook
